3c19dabd1e4309ce75a3e73776d66c6589ceb9b3
RaccoonCode <133838321+BurningBaoTree@users.noreply.github.com>
2023-11-10 11:19:46 +0900

    구현 끝
    고쳐야할것들 : 다시시작하면 AI가 먹통이 됨, 왼쪽 하단 쿨타임 이미지가 제대로 작동하지 않음, 왼쪽 하단 쿨타임 이미지가 다시시작되고 아무 능력도 선택하지 않을경우 전판에 있던 이미지가 그대로 남아있음

--- Assets/Script/UI/GameOver.cs
+++ Assets/Script/UI/GameOver.cs
@@ -8,6 +8,7 @@ public class GameOver : MonoBehaviour
 {
     public TextMeshProUGUI wintext;
     public TextMeshProUGUI Timetext;
+    public TextMeshProUGUI FriendMassage;
 
     public Image skill1;
     public Image skill2;
@@ -47,6 +48,36 @@ public class GameOver : MonoBehaviour
             skill2.sprite = null;
             skill2.color = Color.clear;
         }
+        if (WinOrLose)
+        {
+            if (GameManager.Inst.Dificalty == dificalty.easy)
+            {
+                FriendMassage.text = "친구의 메시지 : 너무 쉬웠나?";
+            }
+            else if (GameManager.Inst.Dificalty == dificalty.middle)
+            {
+                FriendMassage.text = "친구의 메시지 : 오.. 낫베드";
+            }
+            else
+            {
+                FriendMassage.text = "친구의 메시지 : 어캐했누 ㅋㅋㅋㅋㅋ";
+            }
+        }
+        else
+        {
+            if (GameManager.Inst.Dificalty == dificalty.easy)
+            {
+                FriendMassage.text = "친구의 메시지 : 풉킥풉킥";
+            }
+            else if (GameManager.Inst.Dificalty == dificalty.middle)
+            {
+                FriendMassage.text = "친구의 메시지 : 흠... 뭐.. 그럴수 있지..\n 일단 넌 쉬움으로 다시 해라.";
+            }
+            else
+            {
+                FriendMassage.text = "친구의 메시지 : 내가 못깬다 했잖어~";
+            }
+        }
     }
     void getTimeResolt()
     {
@@ -54,7 +85,7 @@ public class GameOver : MonoBehaviour
         mit = (int)timeresult / 60;
         sec = (int)timeresult % 60;
         mil = (int)(timeresult * 1000) % 100;
-        Timetext.text = $"{mit: 00} : {sec: 00} : {mil: 00}";
+        Timetext.text = $"플레이 타임 : {mit: 00} : {sec: 00} : {mil: 00}";
     }
     public void ResetButton()
     {
